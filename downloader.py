a7a4b104949570209d9e3b6413af517f465fb9f1
pultzlucas <pultzlucas@gmail.com>
2023-02-22 15:06:38 -0300

    coloring prints

--- downloader.py
+++ downloader.py
@@ -1,12 +1,14 @@
 from pytube import Playlist
+from termcolor import colored
+
 playlist_url = str(input('Playlist url: '))
 output_path = str(input('Output path: '))
 playlist = Playlist(playlist_url)
 
-print('Downloading playlist: ' + playlist.title)
 print('Number of videos in playlist: %s' % len(playlist.video_urls))
+print('Downloading playlist: ' + playlist.title)
 
 for video in playlist.videos:
     audio = video.streams.filter(only_audio=True).first()
-    print('Downloading -> ' + audio.title)
+    print(colored('Downloading ', 'green') + audio.title)
     audio.download(output_path, audio.title + '.wav')
