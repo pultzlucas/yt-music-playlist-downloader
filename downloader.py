1468465b011c7b71f33cf32b2a54d06ad522b4d3
pultzlucas <pultzlucas@gmail.com>
2023-02-22 15:10:15 -0300

    fix: standard string placements

--- downloader.py
+++ downloader.py
@@ -5,7 +5,7 @@ playlist_url = str(input('Playlist url: '))
 output_path = str(input('Output path: '))
 playlist = Playlist(playlist_url)
 
-print('Number of videos in playlist: %s' % len(playlist.video_urls))
+print('Number of videos in playlist: ' + len(playlist.video_urls))
 print('Downloading playlist: ' + playlist.title)
 
 for video in playlist.videos:
