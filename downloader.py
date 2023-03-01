a6aeebc3f199fb28921a778b7656cd90835e15b4
pultzlucas <pultzlucas@gmail.com>
2023-03-01 15:29:11 -0300

    add progress bar

--- downloader.py
+++ downloader.py
@@ -1,14 +1,24 @@
-from pytube import Playlist
+from pytube import Playlist, YouTube
 from termcolor import colored
+from tqdm import tqdm
 
-playlist_url = str(input('Playlist url: '))
-output_path = str(input('Output path: '))
+playlist_url = 'https://www.youtube.com/playlist?list=PLV1qTAR8p8pjrZNExeqzI8b1fA08tbLpg'#str(input('Playlist url: '))
+output_path = 'o'#str(input('Output path: '))
 playlist = Playlist(playlist_url)
 
-print('Number of videos in playlist: ' + len(playlist.video_urls))
-print('Downloading playlist: ' + playlist.title)
+print('Number of videos in playlist: %s' % len(playlist.video_urls))
+print(colored('Downloading playlist: ', 'yellow') + playlist.title)
 
-for video in playlist.videos:
-    audio = video.streams.filter(only_audio=True).first()
-    print(colored('Downloading ', 'green') + audio.title)
-    audio.download(output_path, audio.title + '.wav')
+
+for video_url in playlist.video_urls:
+    def progress_callback(stream, data_chunk: bytes, bytes_remaining: int):
+        pbar.update(len(data_chunk))
+        
+    video = YouTube(video_url, on_progress_callback=progress_callback)
+    print(colored('Downloading ', 'green') + video.title)
+    
+    # stream = video.streams.get_highest_resolution()
+    stream = video.streams.get_audio_only()
+    pbar = tqdm(total=stream.filesize, unit="bytes", ascii=' >=')
+    stream.download(output_path, stream.title + '.mp3')
+    pbar.close()
