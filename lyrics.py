a6b877da79fc4a35b3cd73db64f20020dbff96b2
pultzlucas <pultzlucas@gmail.com>
2023-03-10 14:44:02 -0300

    feature: integrate with musixmatch api

--- lyrics.py
+++ lyrics.py
@@ -3,49 +3,49 @@ from requests_html import HTMLSession
 import requests
 import os
 import shutil
+import json
+
 
 output_dir = 'Lyrics'
 
-def get_lyrics_url(name):
-    url = f'https://www.vagalume.com.br/search?q={name}'
-    html = requests.get(url).content
-    session = HTMLSession()
-    r = session.get(url)
-    r.html.render()
-    for a in r.html.find('a'):
-        if 'class' in a.attrs and a.attrs['class'][0] == 'gs-title':
-            return a.attrs['href']        
+BASE_URL = 'https://api.musixmatch.com/ws/1.1'
+API_KEY = '29857e86424cd1ed4941b631e2fa8651'
+
 
 def get_lyrics(song_name):
-    url = get_lyrics_url(song_name)
-    print(url)
-    
-    return
-    lyrics_html = requests.get(url)    
-    soup = BeautifulSoup(lyrics_html.content, 'html.parser')
-    
-    
-    # soup = soup.find('div', class_='cnt-letra')
-    # soup = soup.find_all('p')
-    # paragraphs = []
-    # for p in soup:
-    #     paragraph = []
-    #     for line in str(p)[3:-4].split('<br/>'):
-    #         paragraph.append(line)
-    #     paragraphs.append('\n'.join(paragraph))
-    # return '\n'.join(paragraphs)
+    req = requests.get(
+        f'{BASE_URL}/track.search?q_lyrics={song_name}&apikey={API_KEY}')
+    res = json.loads(req.content)
+    track_list = res['message']['body']['track_list']
+
+    if len(track_list) == 0:
+        return None
+
+    track_id = track_list[0]['track']['track_id']
+    req = requests.get(
+        f'{BASE_URL}/track.lyrics.get?track_id={track_id}&apikey={API_KEY}')
+    res = json.loads(req.content)
+
+    return res['message']['body']['lyrics']['lyrics_body']
+
 
 def write_lyric_file(song_name, lyrics):
     with open(f'Lyrics/{song_name}.txt', "w", encoding="utf-8") as f:
         f.write(lyrics)
         f.close()
-        
+
+
 def main():
     os.mkdir(output_dir)
     songs = os.listdir('o')
-    songs_lyrics = [get_lyrics(song[0:-4]) for song in songs]
-    
-    
+    songs_lyrics = [
+        {'name': song, 'lyrics': get_lyrics(song[0:-4])} for song in songs]
+
+    for song in songs_lyrics:
+        write_lyric_file(song['name'], str(song['lyrics']))
+
+
 if __name__ == '__main__':
-    if os.path.exists(output_dir): shutil.rmtree(output_dir)
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
     main()
