ec94063ec1ead28afea8f58c994ff7bf739aad3d
pultzlucas <pultzlucas@gmail.com>
2023-03-10 13:34:59 -0300

    feature: getting songs lyrics

--- downloader.py
+++ downloader.py
@@ -6,26 +6,12 @@ from concurrent.futures.process import ProcessPoolExecutor as Executor
 import os
 import shutil
 
-
-# str(input('Playlist url: '))
-playlist_url = 'https://www.youtube.com/playlist?list=PLV1qTAR8p8piVBzQncqjR7srgXuyUeQYU'
+playlist_url = 'https://www.youtube.com/playlist?list=PLV1qTAR8p8pjrZNExeqzI8b1fA08tbLpg' # str(input('Playlist url: '))
 output_path = 'o'  # str(input('Output path: '))
 playlist = Playlist(playlist_url)
 
 print('Number of videos in playlist: %s' % len(playlist.video_urls))
 print(colored('Downloading playlist: ', 'yellow') + playlist.title)
-
-
-# def progress_callback(arg, data_chunk: bytes, arg2):
-#     pbar.update(len(data_chunk))
-
-
-# def completed_callback(stream, arg2):
-#     print(arg2)
-#     clip = mp.VideoFileClip(
-#         output_path + '/' + stream.title + '.mp4').subclip(0, 20)
-#     clip.audio.write_audiofile(output_path + '/' + stream.title + ".mp3")
-
 
 def download_song(video_url):
     video = YouTube(video_url)
@@ -33,16 +19,21 @@ def download_song(video_url):
 
     stream = video.streams.get_highest_resolution()
 
-    filename = stream.title + '.mp4'
+    filename = f'{stream.title}.mp4'
     stream.download(output_path, filename)
 
-    clip = mp.VideoFileClip(output_path + '/' + filename)
-    clip.audio.write_audiofile(output_path + '/' + stream.title + '.mp3')
+    clip = mp.VideoFileClip(f'{output_path}/{filename}')
+    clip.audio.write_audiofile(f'{output_path}/{stream.title}.mp3', logger=None)
+
+    os.remove(os.path.realpath(f'./{output_path}/{filename}'))
+    # print('*'*20)
+    # print(os.path.realpath(f'./{output_path}/{filename}'))
+    # print('*'*20)
 
 if __name__ == '__main__':
-    # if os.path.exists('o'): shutil.rmtree('o')
-    # with Executor() as executor:
-    #     executor.map(download_song, playlist.video_urls)
+    if os.path.exists('o'): shutil.rmtree('o')
+    with Executor() as executor:
+        executor.map(download_song, playlist.video_urls)
     videos = [f for f in os.listdir(output_path) if os.path.isfile(os.path.join(output_path, f)) and os.path.splitext(f)[-1] == '.mp4']
     for video in videos: 
         os.remove(output_path + '/' + video)
--- lyrics.py
+++ lyrics.py
@@ -1,9 +1,14 @@
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 import requests
+import os
+import shutil
+
+output_dir = 'Lyrics'
 
 def get_lyrics_url(name):
-    url = f'https://www.letras.mus.br/?q={name}'
+    url = f'https://www.vagalume.com.br/search?q={name}'
+    html = requests.get(url).content
     session = HTMLSession()
     r = session.get(url)
     r.html.render()
@@ -11,12 +16,36 @@ def get_lyrics_url(name):
         if 'class' in a.attrs and a.attrs['class'][0] == 'gs-title':
             return a.attrs['href']        
 
-link = get_lyrics_url('Me encontra')
-response = requests.get(link)
+def get_lyrics(song_name):
+    url = get_lyrics_url(song_name)
+    print(url)
+    
+    return
+    lyrics_html = requests.get(url)    
+    soup = BeautifulSoup(lyrics_html.content, 'html.parser')
+    
+    
+    # soup = soup.find('div', class_='cnt-letra')
+    # soup = soup.find_all('p')
+    # paragraphs = []
+    # for p in soup:
+    #     paragraph = []
+    #     for line in str(p)[3:-4].split('<br/>'):
+    #         paragraph.append(line)
+    #     paragraphs.append('\n'.join(paragraph))
+    # return '\n'.join(paragraphs)
 
-soup = BeautifulSoup(response.content, 'html.parser')
-soup = soup.find('div', class_='cnt-letra')
-soup = soup.find_all('p')
-
-for p in soup:
-    print(p.text)
+def write_lyric_file(song_name, lyrics):
+    with open(f'Lyrics/{song_name}.txt', "w", encoding="utf-8") as f:
+        f.write(lyrics)
+        f.close()
+        
+def main():
+    os.mkdir(output_dir)
+    songs = os.listdir('o')
+    songs_lyrics = [get_lyrics(song[0:-4]) for song in songs]
+    
+    
+if __name__ == '__main__':
+    if os.path.exists(output_dir): shutil.rmtree(output_dir)
+    main()
